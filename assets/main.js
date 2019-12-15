b28b339c867c7d17b39f43ec14f38f562322c09d
Allan96 <allan.souza.silva@usp.br>
2019-12-14 21:19:50 -0300

    Consumindo API Matera

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('button').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://varcom-mp-api-01.matera.com/v1/accounts",
+            headers: {
+                'Transaction-Hash': 'f1cb40acb0c3863f74f9f4fb41558dd1847beece9695a827cf7a0d99d8c8ed97',
+                'Api-Access-Key': ' 813F6952-D615-4E5C-9547-C31ABED75895x',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            type: "POST",
+            data: "data",
+            dataType: "json",
+            success: function(response) {
+                console.log(response);
+            }
+        });
+
+    });
+});
